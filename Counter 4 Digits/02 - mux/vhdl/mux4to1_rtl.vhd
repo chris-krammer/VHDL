ba9099c3df92e6de5333315c56852a76bf7a6813
Chris <nonono@guessme.org>
2019-07-24 12:38:08 +0200

    Update mux4to1_rtl.vhd

--- Counter 4 Digits/02 - mux/vhdl/mux4to1_rtl.vhd
+++ Counter 4 Digits/02 - mux/vhdl/mux4to1_rtl.vhd
@@ -29,14 +29,10 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 architecture rtl of mux4to1 is
-  
   begin
 
-
   p_comb : process(SEL_i, IN0_i, IN1_i, IN2_i, IN3_i) is
-
     begin
-
       case SEL_i is
         when "00" => OUT_o <= IN0_i;
         when "01" => OUT_o <= IN1_i;
