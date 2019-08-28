9cd8e1448f7dc9a9f935dd02efb1af3c1bb5e70a
Chris <nonono@guessme.org>
2019-08-28 14:09:36 +0200

    Update debouncer_rtl.vhd

--- Counter/debouncer/vhdl/debouncer_rtl.vhd
+++ Counter/debouncer/vhdl/debouncer_rtl.vhd
@@ -39,34 +39,34 @@ architecture rtl of debouncer is
 
   constant c_COUNTERTARGET  : integer range 0 to g_SOURCEHZ := (g_SOURCEHZ / (1000 / g_DEBTIME ));
 
-  SIGNAL s_flipflops    : STD_LOGIC_VECTOR(1 DOWNTO 0); --input flip flops
-  SIGNAL s_counter_set  : STD_LOGIC;                    --sync reset to zero
-  SIGNAL s_OUT          : STD_LOGIC;
+  signal s_flipflops    : STD_LOGIC_VECTOR(1 downto 0); --input flip flops
+  signal s_counter_set  : STD_LOGIC;                    --sync reset to zero
+  signal s_OUT          : STD_LOGIC;
 
 begin
 
   s_counter_set <= s_flipflops(0) xor s_flipflops(1);  --determine when to start/reset counter
 
-  PROCESS(CLK_i, RST_i)
-    VARIABLE v_count :  INTEGER RANGE 0 TO c_COUNTERTARGET;  --counter for timing
-  BEGIN
-    IF(RST_i = g_RSTACTIVE) THEN              --reset
-      s_flipflops(1 DOWNTO 0) <= "00";          --clear input flipflops
+  process(CLK_i, RST_i)
+    variable v_count :  integer range 0 to c_COUNTERTARGET;  --counter for timing
+  begin
+    if(RST_i = g_RSTACTIVE) then              --reset
+      s_flipflops(1 downto 0) <= "00";          --clear input flipflops
       s_OUT <= '0';                          --clear result register
 
-    ELSIF(CLK_i'EVENT and CLK_i = '1') THEN   --rising clock edge
+    elsif(CLK_i'event and CLK_i = '1') then   --rising clock edge
       s_flipflops(0) <= IN_i;                 --store button value in 1st flipflop
       s_flipflops(1) <= s_flipflops(0);       --store 1st flipflop value in 2nd flipflop
 
-      If(s_counter_set = '1') THEN            --reset counter because input is changing
+      if(s_counter_set = '1') then            --reset counter because input is changing
         v_count := 0;                         --clear the counter
-      ELSIF(v_count < c_COUNTERTARGET) THEN   --stable input time is not yet met
+      elsif(v_count < c_COUNTERTARGET) then   --stable input time is not yet met
         v_count := v_count + 1;               --increment counter
-      ELSE                                    --stable input time is met
+      else                                    --stable input time is met
         s_OUT <= s_flipflops(1);              --output the stable value
-      END IF;
-    END IF;
-  END PROCESS;
+      end if;
+    end if;
+  end process;
 
 -- Assign Signals to Ouputs
 OUT_o <=  s_OUT;
